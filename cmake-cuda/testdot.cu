4d34795313514f1f163b79fe1bde1d2d80372a5e
Edoardo Pasca <edo.paskino@gmail.com>
2021-04-02 23:11:05 +0100

    calculate the Leibnitz series for pi

--- cmake-cuda/testdot.cu
+++ cmake-cuda/testdot.cu
@@ -2,12 +2,15 @@
 #include <chrono>
 #include <limits>
 #include <cmath>
+
 #ifdef _OPENMP
 #include <omp.h>
 #endif
+
+#ifdef CUDABLAS
 #include <cuda_runtime.h>
 #include "cublas_v2.h"
-
+#endif
 int main(int argc, char **argv){
 
   if (argc != 3) {
@@ -32,8 +35,11 @@ int main(int argc, char **argv){
 
 
   double *times = new double[nSamples];
-  double *A = new double[n];
-  double *B = new double[n];
+  double *pi = new double[nSamples];
+  double *diff = new double[nSamples];
+  int N = n+nSamples;
+  double *A = new double[N];
+  double *B = new double[N];
   double tmin=std::numeric_limits<double>::max();
   double tmax=0.0;
   double z;
@@ -42,61 +48,69 @@ int main(int argc, char **argv){
   cublasHandle_t handle;
   double* devPtrA;
   double* devPtrB;
-    if (!A) {
-        printf ("host memory allocation failed");
-        return EXIT_FAILURE;
-    }
-    cudaStat = cudaMalloc ((void**)&devPtrA, n*sizeof(*A));
-    if (cudaStat != cudaSuccess) {
-        printf ("device memory allocation failed");
-        return EXIT_FAILURE;
-    }
-    cudaStat = cudaMalloc ((void**)&devPtrB, n*sizeof(*B));
-    if (cudaStat != cudaSuccess) {
-        printf ("device memory allocation failed");
-        return EXIT_FAILURE;
-    }
-    stat = cublasCreate(&handle);
-    if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("CUBLAS initialization failed\n");
-        return EXIT_FAILURE;
-    }
-for (int i=1; i<=nSamples; ++i) {
-    for (int j=0; j<n; ++j) {
-      A[j] = (double) i * (double)(j+1)/(double) n;
-      B[j] = (double) i * ((double)j+1.5)/(double) n;
-    //  std::cout << A[j] << " " << B[j] << std::endl;
-    }
   
-    stat = cublasSetVector(n, sizeof(double),
+  // set arrays values
+  int sign = 1;
+  for (int j=0; j<N; ++j) {
+    A[j] = 4./(double)(2*j+1);
+    B[j] = (double)(sign);
+    sign *= -1;
+  }
+#ifdef CUDABLAS
+  cudaStat = cudaMalloc ((void**)&devPtrA, (N)*sizeof(*A));
+  if (cudaStat != cudaSuccess) {
+    std::cout << "device memory allocation failed" << std::endl;
+    return EXIT_FAILURE;
+  }
+  cudaStat = cudaMalloc ((void**)&devPtrB, (N)*sizeof(*B));
+  if (cudaStat != cudaSuccess) {
+    std::cout << "device memory allocation failed" << std::endl;
+    return EXIT_FAILURE;
+  }
+  stat = cublasCreate(&handle);
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "CUBLAS initialization failed" << std::endl;
+      return EXIT_FAILURE;
+  }
+#endif
+  for (int i=0; i<nSamples; ++i) {
+
+#ifdef CUDABLAS
+    stat = cublasSetVector(n+i, sizeof(double),
                 A, 1, devPtrA , 1);
     if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("CUBLAS initialization failed\n");
+      std::cout << "CUBLAS SetVector failed" << std::endl;
         return EXIT_FAILURE;
     }
-    stat = cublasSetVector(n, sizeof(double),
+    stat = cublasSetVector(n+i, sizeof(double),
                 B, 1, devPtrB , 1);
     if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("CUBLAS initialization failed\n");
+      std::cout << "CUBLAS SetVector failed" << std::endl;
         return EXIT_FAILURE;
     }
+#endif
 
     const auto start = std::chrono::high_resolution_clock::now();
 #ifdef WITH_BLAS
-    z=cblas_ddot(n,A,1,B,1);
-#else    
-    stat = cublasDdot(handle, n, devPtrA, 1, devPtrB, 1, &z);
+    z=cblas_ddot(n+i,A,1,B,1);
+#else
+#ifdef CUDABLAS
+    stat = cublasDdot(handle, n+i, devPtrA, 1, devPtrB, 1, &z);
     if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("CUBLAS initialization failed\n");
+      std::cout << "CUBLAS Ddot failed" << std::endl;
         return EXIT_FAILURE;
     }
+#endif
 //    z=dot(A,B,n);
 #endif    
-    times[i-1] = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
-    if (tmin>times[i-1]) tmin=times[i-1];
-    if (tmax<times[i-1]) tmax=times[i-1];
+    pi[i] = z;
+    diff[i] = M_PI - z;
+    times[i] = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
+    if (tmin>times[i]) tmin=times[i];
+    if (tmax<times[i]) tmax=times[i];
 
   }
+  
   double timeAvg = 0.0;
   for(int i=0; i<nSamples; ++i) {
     timeAvg+=times[i];
@@ -111,20 +125,24 @@ for (int i=1; i<=nSamples; ++i) {
 
   std::cout<<"#Loop  |       Size|        Time (s)|"<<std::endl;
   for(int i=0; i<nSamples; ++i) {
-    std::cout<<i+1<<" "<<n<<" "<<times[i]<<std::endl;
+    // std::cout<<i+1<<" "<<n<<" "<<times[i]<<std::endl;
+    std::cout<<i<<" "<<pi[i]<<" |z - pi| = "<< fabs(diff[i]) <<" < "<< (4./(double)(2*(i+n)+3)) << " " << times[i]<<"s "<<std::endl;
   }
   std::cout<<"Last inner product: "<<z<<std::endl;
   std::cout<<"Summary:"<<std::endl;
   std::cout<<"#Size  n    |  Avg. Time (s) |   Min. Time(s) |   Max. Time(s) | σ Time(s)"<<std::endl;
   std::cout<<n<<" "<<timeAvg<<" "<<tmin<<" "<<tmax<<" "<<sig<<std::endl;
-
+  
+#ifdef CUDABLAS
   cudaFree (devPtrA);
   cudaFree (devPtrB);
   cublasDestroy(handle);
+#endif
+
   delete[] times;
   delete[] A;
   delete[] B;
-
+  std::cout<<"Last inner product: "<<z<<std::endl;
   return 0;
-}
+  }
 
